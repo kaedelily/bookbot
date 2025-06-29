f0c21bf94bf1f174e1ccd3a4c122653b7589bb2c
kaedelily <svlibertini1998@gmail.com>
2025-06-29 18:04:20 -0400

    main

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(book):
+     with open(book) as f: # Open the book file: used in main function in line 2
+            return f.read() # Read the content of the book file: replaces the previous get_book_text function
+
+def main():
+    book_text = get_book_text(books/frankenstein.txt) # type: ignore
+    print(book_text)
+
+main()
