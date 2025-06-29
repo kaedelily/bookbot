cbf9d3684cf9ba7a6adcf6248a12d7598c35682f
kaedelily <svlibertini1998@gmail.com>
2025-06-29 18:18:54 -0400

    read file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def get_book_text(book):
-     with open(book) as f: # Open the book file: used in main function in line 2
+def get_book_text(path):
+     with open(path) as f: # Open the book file: used in main function in line 2
             return f.read() # Read the content of the book file: replaces the previous get_book_text function
 
 def main():
