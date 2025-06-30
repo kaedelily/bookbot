cb31dcb2d9e47bb0d50d88ae195526ceb1434b13
kaedelily <svlibertini1998@gmail.com>
2025-06-29 23:31:21 -0400

    analysis

--- main.py
+++ main.py
@@ -17,8 +17,6 @@ def main():
     char_list = sort_characters(book_text)
     print("============ BOOKBOT ============") # Print the header for the output
     print(f"Analyzing book found at {book_text}...") # Print the path of the book being analyzed
-    print("----------- Word Count ----------") # Print the header for the word count section
-    print(f"Found {words} total words") # Print the number of words found in the document"
     print("--------- Character Count -------") # Print the header for the character count section
     for item in char_list:
          if item["char"].isalpha():
