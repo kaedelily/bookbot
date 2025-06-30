b11e057c125a48baeea0c5efee372ef877efd45a
kaedelily <svlibertini1998@gmail.com>
2025-06-29 23:25:24 -0400

    sort chars

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import character_count
+from stats import sort_characters
 
 def get_book_text(path):
      with open(path) as f: # Open the book file: used in main function in line 2
@@ -9,6 +10,15 @@ def main():
     book_text = get_book_text("books/frankenstein.txt") 
     words = get_num_words(book_text)
     char_num = character_count(book_text)
-    print(char_num)
+    char_list = sort_characters(book_text)
+    print("============ BOOKBOT ============") # Print the header for the output
+    print(f"Analyzing book found at {book_text}...") # Print the path of the book being analyzed
+    print("----------- Word Count ----------") # Print the header for the word count section
+    print(f"Found {words} total words") # Print the number of words found in the document"
+    print("--------- Character Count -------") # Print the header for the character count section
+    for item in char_list:
+         if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}") # Print each character and its count
+    print("============= END ===============") # Print the footer for the output
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,14 @@ def character_count(get_book_text):
         else:
             characters[chr] = 1
     return characters # Return the character count dictionary
+
+def sort_on(character):
+    return character # Return the sorted character count
+def sort_characters(get_book_text):
+    counts = character_count(get_book_text) # Get the character count dictionary
+    char_list = [] # Initialize an empty list to store characters
+    for letter in counts: # Iterate through each character in the character count dictionary
+        num = counts[letter]
+        char_list.append({"char": letter, "num": num}) # Append the character and its count to the list
+    char_list.sort(key=lambda d: d["num"], reverse=True) # Sort the list by character count in descending order
+    return char_list # Return the sorted character list
